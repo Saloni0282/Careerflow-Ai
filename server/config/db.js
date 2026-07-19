36bcedfbab65f96f152aa0f90e6627de4ecb2597
salonishalu2000@gmail.com <salonishalu2000@gmail.com>
2026-07-19 22:00:46 +0530

    added readme and mongodb url

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,22 @@
 import mongoose from 'mongoose';
+// import dns from 'node:dns';
+
+// // Force Node to use Google's DNS servers
+// dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
-  try {
-    const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/careerflow-ai';
-    await mongoose.connect(uri);
-    console.log('[MongoDB] Connected');
-  } catch (error) {
-    error.context = 'MongoDB Connection Error';
-    throw error;
-  }
+    try {
+        const uri =
+            process.env.MONGO_URI ||
+            'mongodb+srv://shalu:shalu@cluster0.tmtrslh.mongodb.net/careerflow-ai?retryWrites=true&w=majority&appName=Cluster0';
+
+        await mongoose.connect(uri);
+
+        console.log('[MongoDB] Connected');
+    } catch (error) {
+        error.context = 'MongoDB Connection Error';
+        throw error;
+    }
 };
 
 export default connectDB;
